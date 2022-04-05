8874a32ec12d16d02c0a8c43cf6b5d9e6e8edebb
Gajam_Jyothsna <jyothsna.gajam@yesgnome.com>
2022-04-05 18:41:09 +0530

    First Person Shooting Game is Initialised and Enemy Spawn Positions is Done.

---
+++ Assets/PlayerSpawn.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+
+    public Transform playerSpawnPoints; //Parent of the SPAWN points
+    Transform[] spawnPositions;
+   public bool isRespawn = false;
+    public bool isSRespawnToggle = false;
+    int k;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPositions = playerSpawnPoints.GetComponentsInChildren<Transform>();
+       // RandomSpawn();
+        Debug.Log(spawnPositions.Length);
+        
+    }
+
+    private int RandomSpawn()
+    {
+        int i = UnityEngine.Random.Range(1, spawnPositions.Length);
+        
+        Debug.Log(spawnPositions[i].transform.position);
+        transform.position = spawnPositions[i].transform.position;
+
+        return (i);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {/*
+        if(isSRespawnToggle !=isRespawn)
+        {
+            RandomSpawn();
+            isRespawn = false;
+        }
+        else
+        {
+            isSRespawnToggle = isRespawn;
+        }*/
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            k = RandomSpawn();
+        }
+        transform.position = spawnPositions[k].transform.position;
+        
+    }
+}
